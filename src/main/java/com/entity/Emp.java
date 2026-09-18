8267cf9cd4dcbf85894fbc96fadc1407c735b3a6
lhb <qdrtyuiop12@qq.com>
2026-09-18 23:10:00 +0800

    1234

---
+++ src/main/java/com/entity/Emp.java
@@ -0,0 +1,108 @@
+package com.entity;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Emp implements Serializable {
+    private Integer empno;
+    private String ename;
+    private String job;
+    private Integer mgr;
+    private LocalDate hiredate;
+    private BigDecimal sal;
+    private BigDecimal comm;
+    private Integer deptno;
+
+    public Emp() {
+    }
+
+    public Emp(Integer empno, String ename, String job, Integer mgr, LocalDate hiredate, BigDecimal sal, BigDecimal comm, Integer deptno) {
+        this.empno = empno;
+        this.ename = ename;
+        this.job = job;
+        this.mgr = mgr;
+        this.hiredate = hiredate;
+        this.sal = sal;
+        this.comm = comm;
+        this.deptno = deptno;
+    }
+
+    public Integer getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(Integer empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public Integer getMgr() {
+        return mgr;
+    }
+
+    public void setMgr(Integer mgr) {
+        this.mgr = mgr;
+    }
+
+    public LocalDate getHiredate() {
+        return hiredate;
+    }
+
+    public void setHiredate(LocalDate hiredate) {
+        this.hiredate = hiredate;
+    }
+
+    public BigDecimal getSal() {
+        return sal;
+    }
+
+    public void setSal(BigDecimal sal) {
+        this.sal = sal;
+    }
+
+    public BigDecimal getComm() {
+        return comm;
+    }
+
+    public void setComm(BigDecimal comm) {
+        this.comm = comm;
+    }
+
+    public Integer getDeptno() {
+        return deptno;
+    }
+
+    public void setDeptno(Integer deptno) {
+        this.deptno = deptno;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "empno=" + empno +
+                ", ename='" + ename + '\'' +
+                ", job='" + job + '\'' +
+                ", mgr=" + mgr +
+                ", hiredate=" + hiredate +
+                ", sal=" + sal +
+                ", comm=" + comm +
+                ", deptno=" + deptno +
+                '}';
+    }
+}
